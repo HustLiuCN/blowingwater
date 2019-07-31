ca468510f50260601c5b9b4d2e93ae0ea69960cc
liupeidong <liupeidong@kuaishou.com>
2019-07-31 18:46:25 +0800

    git rm cache

--- build/config.js
+++
@@ -1,9 +0,0 @@
-const env = process.env.NODE_ENV || 'development'
-const isDev = env === 'development'
-const buildType = process.env.BUILD_TYPE || 'prod'
-
-module.exports = {
-	env,
-	isDev,
-	buildType,
-}
